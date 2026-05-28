dbdce300d17e23c883f51c16ec23451e65a52114
Piotr Gawlowicz <piotr.gawlowicz@srs.io>
2026-05-29 00:06:23 +0200

    app: add NTN config update remote command
    Signed-off-by: Piotr Gawlowicz <piotr.gawlowicz@srs.io>

--- apps/units/flexible_o_du/split_helpers/commands/ntn_config_update_remote_command_factory.cpp
+++ apps/units/flexible_o_du/split_helpers/commands/ntn_config_update_remote_command_factory.cpp
@@ -3,14 +3,14 @@
 // Portions of this file may implement 3GPP specifications, which may be subject to additional licensing requirements.
 
 #include "ntn_config_update_remote_command_factory.h"
+#include "apps/units/flexible_o_du/o_du_unit.h"
+#include "ntn_config_update_remote_command.h"
 
 using namespace ocudu;
+using namespace ocudu_ntn;
 
-#ifndef OCUDU_HAS_ENTERPRISE_NTN
-
-void ocudu::add_ntn_config_update_remote_command(application_unit_commands&            commands,
-                                                 ocudu_ntn::ntn_configuration_manager& ntn_manager)
+void ocudu::add_ntn_config_update_remote_command(application_unit_commands& commands,
+                                                 ntn_configuration_manager& ntn_manager)
 {
+  commands.remote.push_back(std::make_unique<ntn_config_update_remote_command>(ntn_manager));
 }
-
-#endif // OCUDU_HAS_ENTERPRISE_NTN
